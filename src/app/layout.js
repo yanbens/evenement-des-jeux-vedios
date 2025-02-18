33e903bec1ae6c538bc02631c868a153ee27a8c8
yalali katia <2724026@collegelacite.ca>
2025-02-17 20:26:56 -0500

    Gabarit du base

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Bonjour",
+  description: "Ici une nouvelle description de notre app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="navbar">Barre d’application</div>
+        <div className="content">{children}</div>
+        <div className="footer">Footer</div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold p-20">Welcome to Next.js</h1>
+    </div>
+  );
+}
