98b7c24a2bdc97faebb16ff9ff04d1cb41210ea4
Yanis <2722882@collegelacite.ca>
2025-02-25 14:43:31 -0500

    finaliser le header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Myheader from"@/components/myheader/Myheader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="navbar">Barre d’application</div>
+        <div className="navbar">
+<Myheader />
+</div>
+
         <div className="content">{children}</div>
         <div className="footer">Footer</div>
       </body>
--- src/components/myheader/Myheader.jsx
+++ src/components/myheader/Myheader.jsx
@@ -1,9 +1,14 @@
-import React from  "react";
+import React from  "react"; 
+import Menu from "./menu/Menu";
+import styles from "./Myheader.module.css"
+import Logo from "./Logo";
 const Myheader = (props) => {
     console.log(props)
-    return (<div classeName={StyleSheet.Myheader}> 
+    return (
+    <div className={styles.Myheader}> 
     <Logo/> 
     <Menu/>
     </div>
     );
 };
+export default Myheader;
