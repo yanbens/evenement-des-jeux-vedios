a14a152fefe74810bf672ebbf5cd9d80cbf3a341
2724735 chekaba elisa <2724735@collegelacite.ca>
2025-02-27 17:58:03 -0500

    ajout membre et page dacceuil

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
-export default function Home() {
+import Member from "../components/member/Member";
+import styles from "../components/member/Member.module.css";
+export default function Accueil() {
+  const teamMembers = [
+    { name: "Jean Dupont", role: "Organisateur", imgSrc: "" },
+    { name: "Sophie Martin", role: "Responsable Logistique", imgSrc: "" },
+    { name: "Alexandre Leroy", role: "Community Manager", imgSrc: "" },
+  ];
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold p-20">Welcome to Next.js</h1>
-    </div>
+    <div className={styles.container}><header className={styles.header}><h1>Bienvenue</h1><p>Découvrez et participez à des événements inoubliables.</p></header><section className={styles.presentation}><div className={styles.presentationText}><p>
+            Les événements de jeux vidéo sont des rassemblements où les
+            passionnés de jeux se rencontrent pour partager leur amour des jeux,
+            participer à des tournois, découvrir les dernières nouveautés et
+            technologies, et rencontrer des développeurs et des influenceurs de
+            l'industrie. Ces événements offrent une expérience immersive et
+            interactive, permettant aux participants de se plonger dans
+            l'univers des jeux vidéo et de vivre des moments inoubliables.
+          </p></div></section><section className={styles.membres}><h2>Notre équipe</h2><div className={styles.membreList}>
+          {teamMembers.map((member, index) => (
+            <Member 
+            key={index}
+            name={member.name}
+            role={member.role}
+            image={member.imgSrc}
+            />
+          ))}
+        </div></section></div>
   );
 }
