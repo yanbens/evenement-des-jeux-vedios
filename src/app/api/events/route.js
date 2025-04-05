1cc2a50cab265f87d0fa738e14ad2ec8575d2062
yalali katia <2724026@collegelacite.ca>
2025-04-04 22:44:35 -0400

    La dernier version de la 2 eme partie

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,33 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+
 const prisma = new PrismaClient();
+
 // GET: Récupérer tous les événements
 export async function GET(req) {
-try {
-const events = await prisma.event.findMany();
-return Response.json(events, { status: 200 });
-} catch (error) {
-return Response.json(
-{ message: "Erreur interne du serveur" },
-{ status: 500 }
-);
+  try {
+    const events = await prisma.event.findMany();
+    return NextResponse.json(events, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Erreur interne du serveur" },
+      { status: 500 }
+    );
+  }
 }
-}
+
 // POST: Ajouter un nouvel événement
 export async function POST(req) {
-    try {
+  try {
     const body = await req.json();
     const { date, title, location, description, imageUrl } = body;
+
     const event = await prisma.event.create({
-    data: { date, title, location, description, imageUrl },
+      data: { date, title, location, description, imageUrl },
     });
+
     return NextResponse.json(event, { status: 201 });
-    } catch (error) {
+  } catch (error) {
     console.error("Server error:", error);
     return NextResponse.json(
-    { message: "Erreur interne du serveur" },
-    { status: 500 }
+      { message: "Erreur interne du serveur" },
+      { status: 500 }
     );
-    }
-    }
-    
+  }
+}
